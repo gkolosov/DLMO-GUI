d079927eb6094c610486dd23959dc46aa7dd075c
gkolosov <germain.96@hotmail.fr>
2022-09-06 21:25:00 -0400

    paths

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # DLMO-GUI
 
-[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/gkolosov/DLMO-GUI/HEAD?labpath=notebooks%2Fexpert_DLMO_interface.ipynb)
+[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/gkolosov/DLMO-GUI/HEAD?labpath=expert_DLMO_interface.ipynb)
--- dlmo_expert_GUI.py
+++ dlmo_expert_GUI.py
@@ -24,8 +24,8 @@ import datetime as dt
 
 
 #PATH_TRUE = "data/melatonin_data_N=261.csv"
-PATH_TEST = "data/melatonin_data_N=261_test.csv"
-PATH_SAVE = "data/expert_labels.pickle"
+PATH_TEST = "../data/melatonin_data_N=261_test.csv"
+PATH_SAVE = "../data/expert_labels.pickle"
 
 PATH = PATH_TEST
 RED_CIRCLE = int("1F534",base=16)
